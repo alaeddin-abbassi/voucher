5b1ee38f19213b85167d19f4d80dbe315b36b44b
Alaeddin Abbassi <alaeddin.abbassi@edeka.de>
2022-03-11 20:04:25 +0100

    rename app and add monogose module

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { VoucherModule } from './voucherModule';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(VoucherModule);
 
   const config = new DocumentBuilder()
     .setTitle('Cats example')
