2eae523dd5328895ced7b330ac2fd7cb73334090
Alaeddin Abbassi <alaeddin.abbassi@edeka.de>
2022-03-12 22:40:35 +0100

    run voucher-api and mongodb in docker

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+# Docker Multistage construction 
+
+### DEV Environmental Science  ###
+FROM node:14.17.3 AS development
+
+#  Navigate to the container working directory 
+WORKDIR /usr/src/app
+#  Copy package.json
+COPY package*.json ./
+
+RUN npm install glob rimraf
+RUN npm install --only=development
+COPY . .
+RUN npm run build
--- src/voucherModule.ts
+++ src/voucherModule.ts
@@ -5,10 +5,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { Voucher, VoucherSchema } from './voucher.schema';
 import { VoucherService } from './voucher.service';
 import { VoucherRepository } from './voucher.repository';
+const url = process.env.MONGO_URL || 'localhost';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/nest'),
+    MongooseModule.forRoot(
+      `mongodb://root:example@${url}:27017/nest?authSource=admin`,
+    ),
     MongooseModule.forFeature([{ name: Voucher.name, schema: VoucherSchema }]),
   ],
   controllers: [VoucherController],
